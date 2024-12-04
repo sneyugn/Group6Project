4253c61f6f115c8a8423a082e0e2f2b8857e5257
Simon Nguyen <snguyen2297@student.miracosta.edu>
2024-12-04 01:57:26 -0800

    Added Shopping Cart, changed around some var names

--- src/main/java/com/cs210/groupproject/MenuController.java
+++ src/main/java/com/cs210/groupproject/MenuController.java
@@ -7,11 +7,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import javafx.scene.text.Font;
-import javafx.fxml.FXML;
 
 
-public class OrderController extends Application {
+import java.util.Objects;
+
+
+public class MenuController extends Application {
 
     @Override
 
@@ -19,22 +20,34 @@ public class OrderController extends Application {
     public void start (Stage primaryStage) {
         Label businessLabel = new Label("Six Sushi");
         Label groupMembersLabel = new Label ("by Simon, Vlad, and Matthew");
+        Button cartButton = new Button();
         Button orderButton = new Button("ORDER");
         Button salesButton = new Button("CHECK SALES");
 
+
         //Image Insert
-        Image image = new Image(getClass().getResourceAsStream("/images/6sushi.png")); // Relative path
+        Image logo = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/6sushi.png"))); // Relative path
+        Image cart = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/shoppingcart.png")));
+
+
 
         //Icon Insert
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/6sushi.png")));
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/6sushi.png"))));
         // Display the image using ImageView
-        ImageView imageView = new ImageView(image);
-
+        ImageView logoView = new ImageView(logo);
+        ImageView cartView = new ImageView(cart);
         //Set image properties
-        imageView.setFitWidth(250);  // Resize width
-        imageView.setFitHeight(250); // Resize height
-        imageView.setPreserveRatio(true);
+        logoView.setFitWidth(250);  // Resize width
+        logoView.setFitHeight(250); // Resize height
+        logoView.setPreserveRatio(true);
 
+        cartView.setFitWidth(35);  // Resize width
+        cartView.setFitHeight(35); // Resize height
+        cartView.setPreserveRatio(true);
+
+        //Set Cart Icon to Button
+        cartButton.setGraphic(cartView);
+        cartButton.setOnAction(e -> System.out.println("Order clicked!"));
 
         // Styles
 
@@ -47,24 +60,35 @@ public class OrderController extends Application {
         businessLabel.setStyle("-fx-font-size: 72px; -fx-font-weight: bold; -fx-text-fill: black; -fx-font-family: Courier New; -fx-font-style: italic;");
         groupMembersLabel.setStyle("-fx-font-size: 12px; -fx-font-weight: bold; -fx-text-fill: black; -fx-font-family: Courier New; -fx-font-style: italic;");
 
+
+        HBox cartBox = new HBox(0,cartView);
         HBox labelBox = new HBox(0, businessLabel);
         HBox groupLabel = new HBox(0, groupMembersLabel);
-        HBox imageBox = new HBox(0,imageView);
+        HBox imageBox = new HBox(0,logoView);
 
-        imageBox.setStyle("-fx-alignment: center;");
+        cartBox.setStyle("-fx-alignment: top-right;");
+        cartBox.setPadding(new Insets(10,10,0,0));
+
         labelBox.setStyle("-fx-alignment: center;");
+        labelBox.setPadding(new Insets(0,0,0,0));
+        imageBox.setStyle("-fx-alignment: center;");
+
         groupLabel.setStyle("-fx-alignment: center;");
-        labelBox.setPadding(new Insets(20,0,0,0));
+        groupLabel.setPadding(new Insets(-10,0,0,0));
+
+
+
         VBox layout = new VBox(10);
 
         layout.setStyle("-fx-background-color: BEIGE; -fx-border-radius: 20; -fx-background-radius: 0;");
         HBox buttonBox = new HBox(15,orderButton,salesButton);
 
         buttonBox.setSpacing(50);
-        buttonBox.setPadding(new Insets(10,0,0,0));
+        buttonBox.setPadding(new Insets(0,0,0,20));
         buttonBox.setStyle("-fx-alignment: center;");
 
         layout.getChildren().addAll(
+                cartBox,
                 labelBox,
                 groupLabel,
                 imageBox,
@@ -72,8 +96,33 @@ public class OrderController extends Application {
         );
 
 
+        //Control what buttons do
+        cartButton.setOnAction(e -> {
+            try {
+                System.out.println("Cart Working!");
+            } catch (Exception ex) {
+                orderButton.setText("Error: " + ex.getMessage());
+            }
+        });
+        orderButton.setOnAction(e -> {
+            try {
+                System.out.println("Order Working!");
+            } catch (Exception ex) {
+                orderButton.setText("Error: " + ex.getMessage());
+            }
+        });
+        salesButton.setOnAction(e -> {
+            try {
+                System.out.println("Sales Working!");
+            } catch (Exception ex) {
+                orderButton.setText("Error: " + ex.getMessage());
+            }
+        });
+
+
 
         Scene scene = new Scene(layout, 400, 500);
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Six Sushi Kiosk");
         primaryStage.show();
